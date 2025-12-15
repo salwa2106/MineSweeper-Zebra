ef0a2f1c7a6fc847394b09265ad120931a0dcc62
salwa2106 <salwanaoum21@gmail.com>
2025-12-15 16:48:05 +0200

    setting controller class is added in controller package

--- src/Controller/SettingsController.java
+++ src/Controller/SettingsController.java
@@ -1,5 +1,13 @@
 package Controller;
 
+import Model.GameSettings;
+import Model.QuestionSettings;
+
 public class SettingsController {
 
+    private final GameSettings gameSettings = new GameSettings();
+    private final QuestionSettings questionSettings = new QuestionSettings();
+
+    public GameSettings getGameSettings() { return gameSettings; }
+    public QuestionSettings getQuestionSettings() { return questionSettings; }
 }
