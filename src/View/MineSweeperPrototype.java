aaf302147cdcdb02f7a201909b5343fbb5baea1a
MAHMODKHJ <mahmod.khateb.1234@gmail.com>
2025-12-21 17:55:16 +0200

    Update MineSweeperPrototype.java

--- src/View/MineSweeperPrototype.java
+++ src/View/MineSweeperPrototype.java
@@ -2462,35 +2462,7 @@ private JPanel[] boardWrappers = new JPanel[2];
 	    }
 	}
 	
-	/* ------------------------------ GRADIENT PANEL CLASS ------------------------------ */
 
-	class GradientPaintPanel extends JPanel {
 
-	    private final Color top;
-	    private final Color bottom;
-
-	    GradientPaintPanel(Color top, Color bottom) {
-	        this.top = top;
-	        this.bottom = bottom;
-	        setOpaque(false);
-	    }
-
-	    @Override
-	    protected void paintComponent(Graphics g) {
-	        super.paintComponent(g);
-	        int h = getHeight();
-	        Graphics2D g2 = (Graphics2D) g;
-	        g2.setPaint(new GradientPaint(
-	                0, 0, top,
-	                0, h, bottom
-	        ));
-	        g2.fillRect(0, 0, getWidth(), h);
-	    }
-	   
-	}
-
-	
-
-	/* ------------------------------ BACKGROUND PANEL ------------------------------ */
 	
 
